ba7e8523f17f49fdaa1b3a81c5f4c23bc486d703
Giorgos Logiotatidis <seadog@sealabs.net>
2011-07-25 19:11:12 +0300

    fetching events, get short url, do the thing

--- hackerspace-tweet-bot.py
+++ hackerspace-tweet-bot.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 #
 # Tweeting bot for hackerspace events
 #
-
 import urllib2
 from datetime import datetime, timedelta
 import json
+import googl
 
+USERNAME = None
+PASSWORD = None
 
 def sameday(d1, d2):
     # hmm maybe there is a function for this, but right now the
@@ -17,8 +21,24 @@ def sameday(d1, d2):
                   map(lambda a: getattr(d1,a) == getattr(d2,a), attr)
                   )
 
+def truncate(string,target):
+    if len(string) > target:
+        return string[:(target-3)] + "..."
+    else:
+        return string
+
+def squawk(username,password,message):
+    """Simple post-to-twitter function"""
+    message = truncate(message,140) # trim message
+
+    data = urllib.urlencode({"status" : message})
+    res = urllib.urlopen("http://%s:%s@identi.ca/api/statuses/update.xml" %\
+                         (username,password), data)
+
+    return res
+
 QUERY = "http://hackerspace.gr/api.php?action=ask&q=[[Category:Events]]" \
-        "&format=json&po=location|Start%20date"
+        "&format=json&po=location|Start%20date|"
 
 today = datetime.now()
 tomorrow = datetime.now() + timedelta(days=1)
@@ -44,9 +64,24 @@ for item in data:
         # cannot parse date, move to next time
         continue
 
+    message = None
     if sameday(today, start_date):
-        print "Today", item['title'], item['properties']['start_date']
+        message = u"Σήμερα στις %02d.%02d: %s %s" %\
+                   (start_date.hour,
+                    start_date.minute,
+                    truncate(item['title'], 140),
+                    googl.shorten(item['uri'])
+                    )
 
     elif sameday(tomorrow, start_date):
-        print "Tomorrow", item['title'], item['properties']['start_date']
+        message = u"Αύριο στις %02d.%02d: %s %s" %\
+                   (start_date.hour,
+                    start_date.minute,
+                    truncate(item['title'], 140),
+                    googl.shorten(item['uri'])
+                    )
+
+    if message:
+        squawk(USERNAME, PASSWORD, message)
+
 
